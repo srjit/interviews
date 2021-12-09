ed09ab8a0f46f1cafe7626f0f082ed4cd55a1ee3
Sreejith S <sreejith2904@gmail.com>
2021-12-09 18:44:39 -0500

    update data reading code

--- apptio/utils.py
+++ apptio/utils.py
@@ -4,6 +4,9 @@ import json
 from datetime import datetime
 import gc
 
+import sqlalchemy
+from sqlalchemy import create_engine, types
+from sqlalchemy.dialects.mysql import LONGTEXT
 
 import pandas as pd
 pd.set_option('display.max_columns', None)
@@ -48,7 +51,10 @@ def validate_and_get_schema(path):
         serv_request_attributes_all = [list(x) for x in df["server_request_keys"].tolist()]
         serv_request_attributes = list(set([item for sublist in serv_request_attributes_all for item in sublist]))
 
-        schema = ["request", "server_request"] + request_attributes + serv_request_attributes
+        schema = request_attributes + serv_request_attributes + ["request_datetime"]
+        
+        columns_to_remove = ["country_code", "requestType", "is_online", "method", "accept_language", "user_map"]        
+        [schema.remove(col) for col in columns_to_remove if col in schema]
         
         df = None
         gc.collect()
@@ -96,11 +102,13 @@ def standardize_data(path, schema):
         serv_request_attributes = list(set([item for sublist in serv_request_attributes_all for item in sublist]))
         for attribute in serv_request_attributes:
             df[attribute] = df["server_request"].apply(lambda x: x[attribute] if attribute in x else None)
-
-        del df["request_keys"]
-        del df["server_request_keys"]
-        del df["request"]
-        del df["server_request"]
+            
+        df["request_datetime"] = df["request_unixtime"].apply(lambda x: datetime.fromtimestamp(x))
+    
+        cols_to_remove = ["request_keys", "server_request_keys", "request", "server_request" ,"request_unixtime", "user_map"]
+        for col in cols_to_remove:
+            if col in df.columns:
+                del df[col]
         
         missing_cols = [x for x in schema if x not in df.columns.tolist()]
         if len(missing_cols) > 0:
@@ -115,19 +123,58 @@ def standardize_data(path, schema):
         return None
     
     
-def standardize_and_store(paths, schema, table, connection, type_map):
+def standardize_and_store(paths, schema, table, connection):
     
-    for path in paths:
+    status_dfs = []
+    df = None
+    tmps = []
+    for path in tqdm(paths):
     
         df = standardize_data(path, schema)
-        df.to_sql(name=table, con=connection, dtype=type_map, index=False, if_exists='append')
         
-        df = None
-        del df
+        #Take care of any new status fields
+        status_columns = ["statusCode", "statusLine", "error"]
+        status_df = df[status_columns]
+        status_df.drop_duplicates(inplace=True)
+        status_dfs.append(status_df)
+        
+        del df["statusLine"]
+        del df["error"]
+        
+        # Take care of User fields - Assumption: One user is using only one device at a time
+        user_columns = ["user_agent", "software_id", "ip", "request_datetime"]
+        user_df = df[user_columns]
+        user_df.drop_duplicates(inplace=True)
+        
+        user_type_map = {x:LONGTEXT for x in user_columns}
+        user_type_map["user_map"] = types.JSON
+        user_df.to_sql(name="users", con=connection, dtype=user_type_map, index=False, if_exists='append')
+
+        # Rest of the columns are events - Take care of that
+        columns_to_ignore_by_event_df = status_columns + user_columns + ["request_unixtime"]
+        event_columns = [x for x in df.columns.tolist() if x not in columns_to_ignore_by_event_df ] + ["statusCode", "ip"]
+        event_df = df[event_columns]
+        
+        event_type_map = {x:LONGTEXT for x in event_columns}
+        event_type_map["responseHeaders"] = types.JSON
+        event_type_map["requestHeaders"] = types.JSON
+        event_df.to_sql(name="events", con=connection, dtype=event_type_map, index=False, if_exists='append')
+        
+        event_df = None
+        user_df = None
+        
+        del event_df
+        del user_df
         
         gc.collect()
+        
+    status = pd.concat(status_dfs)
+    status.drop_duplicates()
+    status_type_map = {x:LONGTEXT for x in schema}
+    status_type_map["user_map"] = types.JSON
+    status.to_sql(name="status", con=connection, dtype=status_type_map, index=False, if_exists='append')
     
-
+    
 def read_gzip_files(gzip_file_paths, swap_path, failed_filename):
     
     '''
